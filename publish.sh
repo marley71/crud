fc52927c3388d6762b2aeb960a86711ed96d86f4
Pier Paolo Ciullo <pierpaolo.ciullo@buongiorno.com>
2020-04-25 11:56:16 +0200

    aggiunto scritp shell publish

--- publish.sh
+++ publish.sh
@@ -1,10 +1,15 @@
 path="$1"
-if [ -z "$path" ]
+if [ -z "$path" ];
 then
   echo "inserire un path"
   exit
 fi
 
+if [ ! -d "$path" ];
+then
+  echo "la directory $path non esiste"
+  exit
+fi
 source /Users/pier/help/bin/activate
 mkdocs build
 cp -Ra site/* $path
